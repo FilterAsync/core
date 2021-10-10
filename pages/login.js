c530767d68fbd92510a8e13ccfb5b46c660bd134
chaosberry <peterpierinakos@gmail.com>
2021-10-10 12:15:54 +0300

    added a proper log in form

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,17 @@ import Head from 'next/head';
 export default function Login() {
     return (
         <>
+            <Head>
+                <title>Core - Log In</title>
+            </Head>
+
             <h1>Log in page</h1>
+            <form>
+                <label>Name:</label>
+                <input type="text" id="name" name="name"></input>
+                <label>Password:</label>
+                <input type="password" id="pass" name="pass"></input>
+            </form>
         </>
     )
 }
