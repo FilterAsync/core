ac887de095a040eb5a0fde652c06ccaebf10c531
chaosberry <peterpierinakos@gmail.com>
2021-10-20 18:15:33 +0300

    finished login algorithm (mostly)

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose';
 // Hashing algorithm package
 import argon2 from 'argon2';
 //
-import mongoose from 'mongoose';
-import User from '../models/userSchema';
+import userSchema from '../models/userSchema';
 
-const uri: string = '';
+const User = mongoose.model('User', userSchema);
+
+const uri: string = process.env.PRIVATEKEY as string; // change in production mode
 
 (async function connectMongoDB() {
     await mongoose.connect(uri);
@@ -24,8 +29,24 @@ route.post('/signup', async (req, res) => {
 
     const user = new User(userObj);
     user.save();
-
+    console.log(userObj);
     res.sendStatus(200);
 });
+
+route.post('/login', async (req, res) => {
+    if (User.findOne({ name: req.body.name }, async (err, user) => {
+        await argon2.verify(user.pass, req.body.pass)
+            .catch((err) => {
+                console.log('Error');
+            });
+        console.log('Success');
+    })) {
+        console.log('Database search succeded');
+    } else {
+        console.log('Database search error');
+    }
+
+    res.sendStatus(200);
+})
 
 export default route;
