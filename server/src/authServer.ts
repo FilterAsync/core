0fcb352bef3c08650de13f0cffc53b6d039ab4bb
chaosberry <peterpierinakos@gmail.com>
2021-10-26 14:57:03 +0300

    added cookies and sessions

--- server/src/authServer.ts
+++ server/src/authServer.ts
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import authRoute from './routes/authRoute';
+import session from 'express-session';
 import morgan from 'morgan';
 import cors from 'cors';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+
+const redisClient = redis.createClient();
+const redisStore = connectRedis(session);
 
 const app = express();
 
@@ -10,7 +19,22 @@ const PORT = 8082;
 app.use(express.json());
 
 app.use(cors({
-	origin: '*'
+	origin: 'http://localhost:3000',
+	credentials: true,
+}));
+app.use(session({
+	secret: 'Session secret',
+	store: new redisStore({
+		client: redisClient,
+		ttl: 24 * 60 * 60
+	}),
+	cookie: {
+		secure: false,
+		httpOnly: true,
+		sameSite: true
+	},
+	saveUninitialized: true,
+	resave: false,
 }));
 
 app.use(morgan('dev'));
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -2,18 +2,33 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import type { RequestHandler } from 'express';
+
 import mongoose from 'mongoose';
 // Hashing algorithm package
 import argon2 from 'argon2';
 //
 import userSchema from '../models/userSchema';
+import session from 'express-session';
 
 const User = mongoose.model('User', userSchema);
 
-const uri: string = process.env.PRIVATEKEY as string; // change in production mode
+const uri: string = process.env.PRIVATEKEY;
+
+const verifySession: RequestHandler = (req, res, next) => {
+    if (req.session.userId) {
+        console.log('authorized 👍');
+        return next();
+    }
+    return console.log('unauthorized 🙀');
+};
 
 (async function connectMongoDB() {
-    await mongoose.connect(uri);
+    try {
+        await mongoose.connect(uri);
+    } catch (err) {
+        return console.log(err);
+    };
     console.log('Connected to database');
 })();
 
@@ -34,27 +49,22 @@ route.post('/signup', async (req, res) => {
 });
 
 route.post('/login', async (req, res) => {
-    const user = await User.findOne({ name: req.body.name });
-    if (!user || !await argon2.verify(user.pass, req.body.pass)) {
-        // exception: user does not exist
-        return;
+    // const user = await User.findOne({ name: req.body.name });
+    // if (!user || !await argon2.verify(user.pass, req.body.pass)) {
+    // exception: user does not exist
+    //    console.log('Incorrect details: ' + req.body)
+    //    return;
+    // };
+
+    req.session.userId = 'something';
+    console.log(req.session.userId);
+    res.json('you are now logged in');
+});
+
+route.post('/verify', (req, res) => {
+    if (!req.session || !req.session.userId) {
+        return res.sendStatus(403);
     };
     res.sendStatus(200);
-
-    // OLD CODE
-    /*
-    if (User.findOne({ name: req.body.name }, async (err, user) => {
-        await argon2.verify(user.pass, req.body.pass)
-            .catch((err) => {
-                console.log('Error');
-            });
-        console.log('Success');
-    })) {
-        console.log('Database search succeded');
-    } else {
-        console.log('Database search error');
-    }
-    */
-})
-
+});
 export default route;
