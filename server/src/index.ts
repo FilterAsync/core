2e215fdf2f35cd63d3326c5bdb34d5ac5358b192
Filter Async <guestpogu129@gmail.com>
2021-11-20 20:12:10 +0700

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,74 +1,75 @@
-import dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import session from "express-session";
-import redis from "redis";
-import connectRedis from "connect-redis";
-import socketio from "socket.io";
-import type { Socket } from "socket.io";
-import { createServer } from "http";
-import mongoose from "mongoose";
-import userSchema from "./models/userSchema";
-import argon2 from "argon2";
-import type { RequestHandler } from "express";
+import express from 'express';
+import cors from 'cors';
+import session from 'express-session';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+import socket from 'socket.io';
+import type { Socket } from 'socket.io';
+import { createServer } from 'http';
+import mongoose from 'mongoose';
+import userSchema from './models/userSchema';
+import argon2 from 'argon2';
+import type { RequestHandler } from 'express';
 
 const PORT = 8080;
 
 const route = express.Router();
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 const uri = process.env.PRIVATEKEY;
 
 (async function connectMongoDB() {
-    try {
-        await mongoose.connect(uri);
-    } catch (err) {
-        return console.log(err);
-    }
-    console.log("Connected to database");
+	try {
+		await mongoose.connect(uri);
+	} catch (err) {
+		return console.log(err);
+	}
+	console.log('Connected to database');
 })();
 
 const verifySession: RequestHandler = (req, res, next) => {
-    if (req.session || req.session.userId) {
-        next();
-    }
+	if (req.session || req.session.userId) {
+		next();
+	}
 };
 
-route.post("/signup", async (req, res) => {
-    let userObj = {
-        name: req.body.name,
-        pass: await argon2.hash(req.body.pass),
-        email: req.body.email,
-    };
+route.post('/signup', async (req, res) => {
+	let userObj = {
+		name: req.body.name,
+		pass: await argon2.hash(req.body.pass),
+		email: req.body.email,
+	};
 
-    const user = new User(userObj);
-    user.save((err) => {
-        console.log(err);
-    });
-    res.sendStatus(200);
+	const user = new User(userObj);
+	user.save((err) => {
+		console.log(err);
+	});
+	res.sendStatus(200);
 });
 
-route.post("/login", async (req, res) => {
-    const user = await User.findOne({
-        name: req.body.name,
-    });
-    // if login is incorrect
-    if (!user || !(await argon2.verify(user.pass, req.body.pass))) {
-        console.log("Incorrect details:", req.body);
-        return res.sendStatus(403);
-    }
-    // setting the session's userid to the id from the database
-    req.session.userId = user._id.toString();
-    console.log(req.session);
-    res.sendStatus(200);
+route.post('/login', async (req, res) => {
+	const user = await User.findOne({
+		name: req.body.name,
+	});
+	// if login is incorrect
+	if (!user || !(await argon2.verify(user.pass, req.body.pass))) {
+		console.log('Incorrect details:', req.body);
+		return res.sendStatus(403);
+	}
+	// setting the session's userId to the id from the database
+	req.session.userId = user._id.toString();
+	console.log(req.session);
+	res.sendStatus(200);
 });
 
-route.get("/verify", (req, res) => {
-    if (!req.session || !req.session.userId) {
-        return res.sendStatus(401);
-    }
-    res.sendStatus(200);
+route.get('/verify', (req, res) => {
+	if (!req.session || !req.session.userId) {
+		return res.sendStatus(401);
+	}
+	res.sendStatus(200);
 });
 
 const redisClient = redis.createClient();
@@ -78,77 +79,77 @@ const app = express();
 
 const httpServer = createServer(app).listen(8081);
 
-const io = new socketio.Server(httpServer, {
-    cors: {
-        origin: "http://localhost:443",
-    },
+const io = new socket.Server(httpServer, {
+	cors: {
+		origin: 'http://localhost:443',
+	},
 });
 
 interface ISocket extends Socket {
-    name?: string;
+	name?: string;
 }
 
 const verifyUsername = (socket: ISocket, next) => {
-    console.log(socket.handshake.auth.name);
-    const name = socket.handshake.auth.name;
-    if (!name) {
-        return next(new Error("invalid username"));
-    }
-    socket.name = name;
-    console.log(socket.name);
-    next();
+	console.log(socket.handshake.auth.name);
+	const name = socket.handshake.auth.name;
+	if (!name) {
+		return next(new Error('invalid username'));
+	}
+	socket.name = name;
+	console.log(socket.name);
+	next();
 };
 
 io.use(verifyUsername);
 
-io.on("connection", (socket: ISocket) => {
-    console.log("User connected:", socket.id);
-    socket.emit("connected", socket.id);
+io.on('connection', (socket: ISocket) => {
+	console.log('User connected:', socket.id);
+	socket.emit('connected', socket.id);
 
-    socket.on("messagepacket", (packet) => {
-        if (socket.name) {
-            return io.sockets.emit("servermessage", {
-                packet: packet,
-                id: socket.id,
-            });
-        }
-    });
+	socket.on('messagepacket', (packet) => {
+		if (socket.name) {
+			return io.sockets.emit('servermessage', {
+				packet: packet,
+				id: socket.id,
+			});
+		}
+	});
 });
 
 app.use(express.json());
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+	express.urlencoded({
+		extended: true,
+	}),
 );
 
 app.use(
-    cors({
-        origin: "http://localhost:443",
-        credentials: true,
-    })
+	cors({
+		origin: 'http://localhost:443',
+		credentials: true,
+	}),
 );
 
 app.use(
-    session({
-        secret: "Session secret",
-        store: new redisStore({
-            client: redisClient,
-            ttl: 24 * 60 * 60,
-        }),
-        name: "session",
-        cookie: {
-            secure: false,
-            httpOnly: true,
-        },
-        saveUninitialized: true,
-        resave: false,
-    })
+	session({
+		secret: 'Session secret',
+		store: new redisStore({
+			client: redisClient,
+			ttl: 24 * 60 * 60,
+		}),
+		name: 'session',
+		cookie: {
+			secure: false,
+			httpOnly: true,
+		},
+		saveUninitialized: true,
+		resave: false,
+	}),
 );
 
-app.use("/api", route);
+app.use('/api', route);
 
 app.listen(PORT, () => {
-    console.log("Main server started on port", PORT);
+	console.log('Main server started on port', PORT);
 });
